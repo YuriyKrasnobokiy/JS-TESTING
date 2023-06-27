d207fa32fce4f44fd74375dae804cebf1f402d80
Yuriy Krasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2023-06-27 18:10:40 +0300

    add some task in module 3.1

--- js-tests.js
+++ js-tests.js
@@ -1809,3 +1809,128 @@
 
 // user.age = 100; // неспрацює
 // console.log(user);
+
+// ======================Практика з записаної лекції=====================================
+
+// //-----------------task 1--------------------------------------
+// Напиши скрипт який для об'єкта user послідовно :
+// додає поле mood зі значенням 'happy'
+// замінює значення hobby на 'skydiving'
+// замінює значення premium на false
+// виводить вміст об'єкта user у форматі ключ: значення використовуючи Object.keys() та for...of
+
+// const user = {
+//   name: "Mango",
+//   age: 20,
+//   hobby: "html",
+//   premium: true,
+// };
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user["premium"] = false;
+// // або
+// const key = "premium";
+// user[key] = false;
+// console.log(user);
+
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//   console.log(user[key]);
+// }
+// console.log(keys);
+
+// ------------------------------------додавання об'єкту в об'єкт----------
+
+// const user = {
+//   name: "Mango",
+//   age: 20,
+//   hobby: "html",
+//   premium: true,
+// };
+
+// user.skills = {
+//   run: true,
+//   swim: false,
+// };
+// console.log(user.skills);
+// console.log(user);
+
+// //-----------------task 2 метод Object.values--------------------------------------
+// У нас є об'єкт де збуріг зарплати ношої команди. Напишіть код для підсумовування всіх зарплат і збережіть результат у змінній sum. Повинно вийти 390. Якщо об'єкт salaries порожній, то результат має бути 0.
+
+// const salaries = {
+//   john: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+
+// -----for...in----
+// let sum = 0;
+
+// for (const key in salaries) {
+//   sum += salaries[key];
+//   console.log(sum);
+// }
+
+// -----for...of----
+// let sum = 0;
+
+// const values = Object.values(salaries);
+// console.log(values);
+// for (const value of values) {
+//   sum += value;
+// }
+// console.log(sum);
+
+// -----------------task 3 - Масив Об'єктів--------------------------------------
+// Напишіть ф-цію calcTotalPrice(stones, stoneName), яка приймає масив об'єктів та рядок з назвою каменю. Ф-ція рахує і повертає загальну вартість каміння з таким ім'ямб ціною та к-тю з об'єкта.
+
+// const stones = [
+//   {
+//     name: "Смарагд",
+//     price: 1300,
+//     quantity: 4,
+//   },
+//   {
+//     name: "Діамант",
+//     price: 2700,
+//     quantity: 3,
+//   },
+//   {
+//     name: "Сапфір",
+//     price: 400,
+//     quantity: 7,
+//   },
+//   {
+//     name: "Щебінь",
+//     price: 200,
+//     quantity: 2,
+//   },
+// ];
+
+// function calcTotalPrice(stones, stoneName) {
+//   for (const stone of stones) {
+//     if (stone.name === stoneName) {
+//       return stone.price * stone.quantity;
+//     }
+//     console.log(stone.name);
+//   }
+//   return "Empty";
+// }
+
+// console.log(calcTotalPrice(stones, "Щебінь"));
+
+// // -----------------task 3 - комплексні завдання--------------------------------------
+// Напиши скрипт управління особистим кабінетом інтернет банку. Є об'єкт account в якому необхідно реалізувати методи для роботи з балансом та історією транзакцій.
+
+// * Типів транзакцій всього два.
+// * можна покласти гроші або зняти з рахунку.
+
+// const Transaction = {
+//   DEPOSIT: "deposit",
+//   WITHDRAW: "withdraw",
+// };
+
+// * кожна транзакція це об'єкт з із вл-ми: id, type та amount
+//
