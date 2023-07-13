bdba6f49d7b22b28f4670d1be72533cac401100c
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2023-07-13 23:10:18 +0300

    add practice from online lesson module 5.2

--- js-tests-module-5.js
+++ js-tests-module-5.js
@@ -77,7 +77,7 @@
 // const gmailGreeter = greet.bind(gmail);
 // gmailGreeter("Полі"); // "Полі, ласкаво просимо в «Gmail»."
 
-// ====================================Нотатки з записаної лекції=============================================
+// =============================Нотатки з записаної лекції=============================================
 
 // const objA = {
 //   name: "User name",
@@ -714,9 +714,9 @@
 // console.log(null ?? undefined);
 // console.log(undefined ?? null);
 
-// // ============================Модуль 5.2 Прототипи та класи=======================================
+// // =====================Модуль 5.2 Прототипи та класи=======================================
 
-// ----------------ОПП---------
+// ------------------------------ОПП-------------------------
 
 // const employee = {
 //   baseSalary: 30000,
@@ -866,3 +866,402 @@
 // console.log(mango.getEmail()); // mango@hottymail.com
 
 // console.log(mango.#email); //Private field '#email' must be declared in an enclosing class
+
+// ------------Геттери і Сеттери-----------------------
+
+// class User {
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   // Геттер email
+//   get email() {
+//     return this.#email;
+//   }
+
+//   // Сеттер email
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({ name: "Манго", email: "mango@mail.com" });
+
+// console.log(mango.email); // mango@mail.com
+
+// mango.email = "mango@supermail.com";
+
+// console.log(mango.email); // mango@supermail.com
+
+// ---------------Статичні властивості--------------------------
+
+// class User {
+//   // Оголошення та ініціалізація статичної властивості
+//   static Roles = {
+//     ADMIN: "admin",
+//     EDITOR: "editor",
+//   };
+
+//   #email;
+//   #role;
+
+//   constructor({ email, role }) {
+//     this.#email = email;
+//     this.#role = role;
+//   }
+
+//   get role() {
+//     return this.#role;
+//   }
+
+//   set role(newRole) {
+//     this.#role = newRole;
+//   }
+// }
+
+// const mango = new User({
+//   email: "mango@mail.com",
+//   role: User.Roles.ADMIN,
+// });
+
+// console.log(mango.Roles); // undefined
+// console.log(User.Roles); // { ADMIN: "admin", EDITOR: "editor" }
+
+// console.log(mango.role); // "admin"
+// mango.role = User.Roles.EDITOR;
+// console.log(mango.role); // "editor"
+
+// ---------------Статичні методи--------------------------
+
+// class User {
+//   static #takenEmails = [];
+
+//   static isEmailTaken(email) {
+//     return User.#takenEmails.includes(email);
+//   }
+
+//   #email;
+
+//   constructor({ email }) {
+//     this.#email = email;
+//     User.#takenEmails.push(email);
+//   }
+// }
+
+// const mango = new User({ email: "mango@mail.com" });
+
+// console.log(User.isEmailTaken("poly@mail.com"));
+// console.log(User.isEmailTaken("mango@mail.com"));
+
+// ---------------Наслідування класів--------------------------
+
+// class User {
+//   #email;
+
+//   constructor(email) {
+//     this.#email = email;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// class ContentEditor extends User {
+//   // Тіло класу ContentEditor
+// }
+
+// const editor = new ContentEditor("mango@mail.com");
+
+// console.log(editor); // { email: "mango@mail.com" }
+// console.log(editor.email); // "mango@mail.com"
+
+// ---------------Конструктор дочірнього класу--------------------------
+
+// class User {
+//   #email;
+
+//   constructor(email) {
+//     this.#email = email;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// class ContentEditor extends User {
+//   constructor({ email, posts }) {
+//     // Виклик конструктора батьківського класу User
+//     super(email);
+//     this.posts = posts;
+//   }
+// }
+
+// const editor = new ContentEditor({ email: "mango@mail.com", posts: [] });
+// console.log(editor); // { email: 'mango@mail.com', posts: [] }
+// console.log(editor.email); // 'mango@mail.com'
+
+// ---------------Методи дочірнього класу--------------------------
+
+// class User {
+//   #email;
+
+//   constructor(email) {
+//     this.#email = email;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// class ContentEditor extends User {
+//   constructor({ email, posts }) {
+//     super(email);
+//     this.posts = posts;
+//   }
+
+//   addPost(post) {
+//     this.posts.push(post);
+//   }
+// }
+
+// const editor = new ContentEditor({ email: "mango@mail.com", posts: [] });
+// console.log(editor); // { email: 'mango@mail.com', posts: [] }
+// console.log(editor.email); // 'mango@mail.com'
+// console.log(editor.posts); // []
+// editor.addPost("post-1");
+// console.log(editor.posts); // ['post-1']
+
+// =============================Нотатки з онлайн лекції=============================================
+
+// // Task-1
+// // Необхідно створити клас Rectangle, який представляє прямокутник. Клас повинен мати приватні властивості width та height, а також публічні гетери та сетери для цих властивостей. Гетери повинні повертати значення властивостей, а сетери повинні дозволяти змінювати значення властивостей з валідацією.
+
+// class Rectangle {
+//   #width;
+//   #height;
+//   constructor({ width, height } = {}) {
+//     this.#width = width;
+//     this.#height = height;
+//   }
+//   get width() {
+//     return this.#width;
+//   }
+
+//   set width(newWidth) {
+//     if (typeof newWidth === "number" && newWidth > 0) {
+//       this.#width = newWidth;
+//     } else {
+//       console.log("Ширина повинна бути числом більшим за 0");
+//     }
+//   }
+//   get height() {
+//     return this.#height;
+//   }
+//   set height(newHeight) {
+//     if (typeof newHeight === "number" && newHeight > 0) {
+//       this.#height = newHeight;
+//     } else {
+//       console.log("Висота повинна бути числом більшим за 0");
+//     }
+//   }
+// }
+
+// const item = new Rectangle({ width: 10, height: 5 });
+
+// console.log(item.width);
+
+// item.width = 12;
+// console.log(item.width);
+
+// console.log(item.height);
+
+// item.height = 7;
+
+// console.log(item.height);
+
+// ------------------------------------------------
+
+// // Task-2
+// // Реалізуйте клас Student, який успадковуватиметься від класу User. Цей клас повинен мати такі властивості:
+// // name приватна властивість (ім'я, успадковується від User),
+// // surname приватна властивість (прізвище, успадковується від User),
+// // year (рік вступу до вузу).
+
+// // Клас повинен мати метод getFullName() (успадковується від User), за допомогою якого можна вивести одночасно ім'я та прізвище студента.
+// // Також клас повинен мати метод getCourse(), який виводитиме поточний курс студента (від 1 до 5, якщо значення перевищує 5  курс виводити що студент являєтсья випускником).
+// // Курс обчислюється так: потрібно від поточного року відняти рік вступу до вузу. Поточний рік отримаєте самостійно (читати документацію!!!).
+// // Приклад ініціалізації екземпляру класа:
+// // const student = new Student('Петрик', 'Пяточкин', 2019);
+
+// class User {
+//   #name;
+//   #surname;
+//   constructor(name, surname) {
+//     this.#name = name;
+//     this.#surname = surname;
+//   }
+
+//   getFullName() {
+//     return `${this.#name} ${this.#surname}`;
+//   }
+// }
+
+// class Student extends User {
+//   constructor(name, surname, year) {
+//     super(name, surname);
+//     this.year = year;
+//   }
+
+//   getCourse() {
+//     const today = new Date();
+//     const currentYear = today.getFullYear();
+//     const diff = currentYear - this.year;
+
+//     if (currentYear < this.year) {
+//       return;
+//     }
+
+//     if (diff > 5) {
+//       return `Студент - випускник`;
+//     }
+//     return `${diff} курс`;
+//   }
+// }
+
+// const student = new Student("Петрик", "П'яточкин", 2019);
+// console.log(student);
+// console.log(student.getFullName()); // поверне "Петрик П'яточкин"
+// console.log(student.getCourse()); // поверне 4 курс
+
+// --------------------------------------------------------------
+
+// // Task-3
+// // Необхідно створити клас BankAccount, який представляє банківський рахунок. Клас повинен мати приватну властивість _balance, яка представляє баланс рахунку. Клас повинен також мати публічні методи deposit та withdraw, які дозволяють здійснювати операції з депозитом та зняттям коштів з рахунку. При цьому _balance повинна бути доступна лише в межах класу BankAccount та його приватних методів.
+
+// class BankAccount {
+//   #balance;
+//   constructor() {
+//     this.#balance = 0;
+//   }
+
+//   deposit(amount) {
+//     if (amount > 0) {
+//       this.#changeBalance(amount);
+//       console.log(`Здійснено депозит ${amount}`);
+//       return;
+//     }
+//     console.log(`Сума має бути більша за 0`);
+//   }
+
+//   withdraw(amount) {
+//     if (amount <= 0) {
+//       console.log(`Сума має бути більша за 0`);
+//     } else if (amount > this.#balance) {
+//       console.log(`Недостатньо коштів на рахунку`);
+//     } else {
+//       this.#changeBalance(-amount);
+//       console.log(`Здійснено зняття ${amount}`);
+//     }
+//   }
+
+//   #changeBalance(amount) {
+//     this.#balance += amount;
+//   }
+// }
+
+// const instance = new BankAccount();
+
+// // ---депозит---
+
+// instance.deposit(0);
+
+// instance.deposit(-1);
+
+// instance.deposit(100);
+// console.log(instance);
+
+// instance.deposit(1000);
+// console.log(instance);
+
+// instance.deposit(200);
+// console.log(instance);
+
+// // ---зняття---
+
+// instance.withdraw(0);
+
+// instance.withdraw(-100);
+
+// instance.withdraw(1400);
+
+// instance.withdraw(200);
+// console.log(instance);
+
+// instance.withdraw(300);
+// console.log(instance);
+
+// instance.withdraw(500);
+// console.log(instance);
+
+// --------------------------------------------------------------
+
+// // Task-4
+// // Необхідно створити клас Hero, який представляє героя з гри. Клас повинен мати публічні властивості name, level та health, а також методи attack та heal, що дозволяють герою атакувати та відновлювати здоров'я відповідно.
+// // Також створи функціонал для підрахунку створених героїв
+
+// // Властивість name має зберігати ім'я героя.
+// // Властивість level має зберігати рівень героя.
+// // Властивість health має зберігати поточний рівень здоров'я героя.
+// // Метод attack наносить пошкодження в розмір 10 одиниць. Метод heal додає до здоров'я героя 10 одиниць.
+
+// class Hero {
+//   static counter = 0;
+//   static addHero() {
+//     this.counter += 1;
+//     console.log("Кількість героїв ", this.counter);
+//   }
+
+//   #level;
+//   constructor(name) {
+//     this.name = name;
+//     this.#level = 1;
+//     this.health = 200;
+//     Hero.addHero();
+//   }
+
+//   attack() {
+//     console.log(`Attack with 10 damage`);
+//   }
+
+//   heal() {
+//     this.health += 10;
+//   }
+// }
+
+// const bloodseker = new Hero("Bloodseker");
+// const bloodseker2 = new Hero("Bloodseker");
+// const bloodseker3 = new Hero("Bloodseker");
+
+// bloodseker.heal();
+// bloodseker.heal();
+// bloodseker.heal();
+// console.log(bloodseker);
+// console.log(Hero.counter);
